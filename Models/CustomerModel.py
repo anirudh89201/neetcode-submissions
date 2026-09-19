18aa6dc4d54fffe39879e4ea78353b22f081be63
AnirudhReddy <44012589+anirudh89201@users.noreply.github.com>
2026-09-20 00:08:31 +0530

    Made changes to Jenkinsfile

--- Models/CustomerModel.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-from pydantic import BaseModel,EmailStr
-
-class CustomerModel(BaseModel):
-    name:str
-    Email:EmailStr
-    
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import uvicorn
 
 from Models import CustomerModel
+
 app = FastAPI()
 
 
@@ -9,8 +10,11 @@ app = FastAPI()
 def health_check():
     return {"message": "Health is good"}
 
+
 @app.post("/add-customer")
-def AddCustomer(body:CustomerModel):
-    return {"message":f"Nice meeting you {body.Email}"}
+def AddCustomer(body: CustomerModel):
+    return {"message": f"Nice meeting you {body.Email}"}
+
+
 if __name__ == "__main__":
-    uvicorn.run(app,host="0.0.0.0",port=8081)
+    uvicorn.run(app, host="0.0.0.0", port=8081)
